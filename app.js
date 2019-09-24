07c0e16772f9608b6c9cd72e9dc8e2e1ed1043d3
Hubert Yang <offspring7890@yahoo.com>
2019-09-24 09:15:48 -0700

    setup sorting and filtering

--- app.js
+++ app.js
@@ -2,6 +2,33 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
-require('./playstore.js')
+const storeData = require('./playstore.js')
 
 app.use(morgan('dev'));
+
+app.get('/apps', (req, res) => {
+  const { sort, genres } = req.query;
+  let results = storeData;
+
+  if(sort) {
+    if (!['Rating', 'App'].includes(sort)) {
+      return res.status(400).send('Sort must of one of rating or app')
+    }
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    })
+  }
+
+  if(genres) {
+    if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+      return res.status(400).send('Genre does not exist')
+    }
+    results = results.filter(name => name.Genres === genres)
+  };
+
+  res.send(results);
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000')
+})
